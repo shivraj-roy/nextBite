3e1f2d470c7df15a10b4d421e0df354dc12409d1
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-27 22:57:47 +0530

    ⇪ Update to use Next.js Image for optimized logo rendering...

--- components/main-header.js
+++ components/main-header.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 import LOGO from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+
 const MainHeader = () => {
    return (
       <header className={classes.header}>
          <Link className={classes.logo} href="/">
-            <img src={LOGO.src} alt="A nextBite logo" />
+            <Image src={LOGO} alt="A nextBite logo" priority />
             <span>NextBite</span>
          </Link>
          <nav className={classes.nav}>
