a8d6a555697b1fae7e749753e130d07c84913743
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-29 00:47:41 +0530

    ≠ Add page not found for anonymous url...

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+   return (
+      <div className="not-found">
+         <h1>Not Found!</h1>
+         <p>Could not find requested resource...</p>
+      </div>
+   );
+};
+export default NotFound;
