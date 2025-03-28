5d93c725355575eee343f55aecd1eeb5e7bb899a
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-29 00:41:06 +0530

    ⚒︎ Add error handling for meal fetching...

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+"use client";
+
+const Error = () => {
+   return (
+      <div className="error">
+         <h1>Something went wrong!</h1>
+         <p>Failed to fetch meals...</p>
+      </div>
+   );
+};
+export default Error;
