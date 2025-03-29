af428a9009b40cda273c94f9c82c99e6f36c62fd
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-29 13:05:45 +0530

    ✴︎ Revamp MealsDetails component to display meal information with enhanced layout...

--- app/meals/[mealsID]/page.js
+++ app/meals/[mealsID]/page.js
@@ -1,8 +1,34 @@
-const MealsDetails = () => {
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+
+const MealsDetails = ({ params }) => {
+   const meal = getMeal(params.mealsID);
+
    return (
-      <div>
-         <h1>MealsDetails</h1>
-      </div>
+      <>
+         <header className={classes.header}>
+            <div className={classes.image}>
+               <Image src={meal.image} alt={meal.slug} fill />
+            </div>
+            <div className={classes.headerText}>
+               <h1>{meal.title}</h1>
+               <p className={classes.creator}>
+                  by{" "}
+                  <a href={`mailto: ${meal.creator_email} `}>{meal.creator}</a>
+               </p>
+               <p className={classes.summary}>{meal.summary}</p>
+            </div>
+         </header>
+         <main>
+            <p
+               className={classes.instructions}
+               dangerouslySetInnerHTML={{
+                  __html: meal.instructions.replace(/\n/g, "<br>"),
+               }}
+            ></p>
+         </main>
+      </>
    );
 };
 export default MealsDetails;
